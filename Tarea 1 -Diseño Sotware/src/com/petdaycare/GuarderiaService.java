6e6cd07cc5d3ab839c7ceebe2d60e3100ffa9c9b
XavierLopezM <xlopezm@espol.edu.ec>
2025-11-01 21:00:33 -0500

    Create GuarderiaService.java
    Agregada clase GuarderiaService que extiende ServicioBase y define compatibilidad de mascotas

---
+++ Tarea 1 -Diseño Sotware/src/com/petdaycare/GuarderiaService.java
@@ -0,0 +1,21 @@
+/**
+ * DESEÑO DE SOFTWARE - Tarea01 SOLID UML
+ * Tema: Sistema PetDayCare
+ * Profesora: David Jurado
+ * @author McDerian (Xavier Lopez M.)
+ * Fecha: 01 nov 2025
+ */
+
+package com.petdaycare;
+
+public class GuarderiaService extends ServicioBase {
+    public GuarderiaService(String id, String nombre, double precioBase) {
+        super(id, nombre, precioBase);
+    }
+
+    @Override
+    public boolean esCompatibleCon(Mascota m) {
+        String tam = m.getTamano().toLowerCase();
+        return tam.equals("pequeño") || tam.equals("mediano");
+    }
+}
