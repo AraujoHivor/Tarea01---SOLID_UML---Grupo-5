f28979140287faa85d47dd0ffab9477f4e8f674e
XavierLopezM <xlopezm@espol.edu.ec>
2025-11-01 20:57:47 -0500

    Create BloqueadorCupos.java
    Agregada clase BloqueadorCupos para manejar reservas

---
+++ Tarea 1 -Diseño Sotware/src/com/petdaycare/BloqueadorCupos.java
@@ -0,0 +1,27 @@
+/**
+ * DESEÑO DE SOFTWARE - Tarea01 SOLID UML
+ * Tema: Sistema PetDayCare
+ * Profesora: David Jurado
+ * @author McDerian (Xavier Lopez M.)
+ * Fecha: 01 nov 2025
+ */
+package com.petdaycare;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BloqueadorCupos {
+    private Map<String, Long> bloqueos = new HashMap<>(); // reservaId -> timestamp
+
+    public void bloquear(String reservaId) {
+        bloqueos.put(reservaId, System.currentTimeMillis());
+    }
+
+    public void liberar(String reservaId) {
+        bloqueos.remove(reservaId);
+    }
+
+    public boolean estaBloqueado(String reservaId) {
+        return bloqueos.containsKey(reservaId);
+    }
+}
